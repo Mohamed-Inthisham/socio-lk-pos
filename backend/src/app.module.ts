c48c342a43e772e0b4549cbe134417c71d512961
Mohamed-Inthisham <black.devil9b@gmail.com>
2026-05-15 11:18:08 +0530

    feat(backend): connect typeorm to postgres with typed config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validateEnv } from './config/validate-env';
+import { TypedConfigModule } from './config/config.module';
+import { TypedConfigService } from './config/typed-config.service';
+import { buildTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnv,
+    }),
+    TypedConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [TypedConfigModule],
+      inject: [TypedConfigService],
+      useFactory: (config: TypedConfigService) => buildTypeOrmConfig(config),
     }),
   ],
   controllers: [AppController],
